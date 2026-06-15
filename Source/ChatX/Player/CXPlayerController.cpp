f1069b93a5bfebca70bced41fd57e98adf27863a
ohchaeyul\olikk <olikkaan9108@gmail.com>
2026-06-15 12:50:06 +0900

    no message

---
+++ Source/ChatX/Player/CXPlayerController.cpp
@@ -0,0 +1,97 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/CXPlayerController.h"
+#include "UI/CXChatInput.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "ChatX.h"
+#include "EngineUtils.h"
+#include "Kismet/GameplayStatics.h"
+#include "Game/CXGameModeBase.h"
+#include "CXPlayerState.h"
+#include "Net/UnrealNetwork.h"
+
+
+
+ACXPlayerController::ACXPlayerController()
+{
+	bReplicates = true;
+}
+
+void ACXPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsLocalController() == false)
+	{
+		return;
+	}
+
+	FInputModeUIOnly InputModeUIOnly;
+	SetInputMode(InputModeUIOnly);
+
+	if (IsValid(ChatInputWidgetClass) == true)
+	{
+		ChatInputWidgetInstance = CreateWidget<UCXChatInput>(this, ChatInputWidgetClass);
+		if (IsValid(ChatInputWidgetInstance) == true)
+		{
+			ChatInputWidgetInstance->AddToViewport();
+		}
+	}
+
+	if (IsValid(NotificationTextWidgetClass) == true)
+	{
+		NotificationTextWidgetInstance = CreateWidget<UUserWidget>(this, NotificationTextWidgetClass);
+		if (IsValid(NotificationTextWidgetInstance) == true)
+		{
+			NotificationTextWidgetInstance->AddToViewport();
+		}
+	}
+
+}
+
+void ACXPlayerController::SetChatMessageString(const FString& InChatMessageString)
+{
+	ChatMessageString = InChatMessageString;
+
+	if (IsLocalController() == true)
+	{
+		ACXPlayerState* CXPS = GetPlayerState<ACXPlayerState>();
+		if (IsValid(CXPS) == true)
+		{
+			FString CombinedMessageString = CXPS->GetPlayerInfoString() + TEXT(": ") + InChatMessageString;
+
+			ServerRPCPrintChatMessageString(CombinedMessageString);
+		}
+	}
+}
+
+void ACXPlayerController::PrintChatMessageString(const FString& InChatMessageString)
+{
+	ChatXFunctionLibrary::MyPrintString(this, InChatMessageString, 10.f);
+}
+
+void ACXPlayerController::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, NotificationText);
+}
+
+void ACXPlayerController::ClientRPCPrintChatMessageString_Implementation(const FString& InChatMessageString)
+{
+	PrintChatMessageString(InChatMessageString);
+}
+
+void ACXPlayerController::ServerRPCPrintChatMessageString_Implementation(const FString& InChatMessageString)
+{
+	AGameModeBase* GM = UGameplayStatics::GetGameMode(this);
+	if (IsValid(GM) == true)
+	{
+		ACXGameModeBase* CXGM = Cast<ACXGameModeBase>(GM);
+		if (IsValid(CXGM) == true)
+		{
+			CXGM->PrintChatMessageString(this, InChatMessageString);
+		}
+	}
+}
---
+++ Source/ChatX/Player/CXPlayerController.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CXPlayerController.generated.h"
+
+class UCXChatInput;
+class UUserWidget;
+
+/**
+ * 
+ */
+UCLASS()
+class CHATX_API ACXPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+
+	ACXPlayerController();
+
+	virtual void BeginPlay() override;
+
+	void SetChatMessageString(const FString& InChatMessageString);
+	
+	void PrintChatMessageString(const FString& InChatMessageString);
+
+	UFUNCTION(Client, Reliable)
+	void ClientRPCPrintChatMessageString(const FString& InChatMessageString);
+
+	UFUNCTION(Server, Reliable)
+	void ServerRPCPrintChatMessageString(const FString& InChatMessageString);
+
+	UPROPERTY(Replicated, BlueprintReadOnly)
+	FText NotificationText;
+
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UCXChatInput> ChatInputWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UCXChatInput> ChatInputWidgetInstance;
+
+	FString ChatMessageString;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> NotificationTextWidgetClass;
+
+	UPROPERTY()
+	TObjectPtr<UUserWidget> NotificationTextWidgetInstance;
+};
